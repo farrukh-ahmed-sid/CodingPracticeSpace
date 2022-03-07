3cf57ee4a1f66ae6444dd14e6e27292aff2403a1
farrukh-ahmed-sid <mr.farrukh.ahmed@gmail.com>
2022-03-07 17:54:52 +0500

    handle all corner cases and issues

--- src/main/java/com/example/tasks/solutions/amazon/hackerrank/ShortestReachInGraph.java
+++ src/main/java/com/example/tasks/solutions/amazon/hackerrank/ShortestReachInGraph.java
@@ -1,24 +1,17 @@
 package com.example.tasks.solutions.amazon.hackerrank;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class ShortestReachInGraph {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        String input = "1\n" +
-                "7 4\n" +
-                "1 2\n" +
-                "1 3\n" +
-                "3 4\n" +
-                "2 5\n" +
-                "2";
-
-        Scanner scanner = new Scanner(input);
+        Scanner scanner = new Scanner(new File("/Users/frahmed/Desktop/input2.txt"));
         String inputLine;
-
         final int EDGE_COST = 6;
-
+        final String SEPARATOR = " ";
         int queries = 0;
         boolean nodesAndEdges = true;
         int nodes = 0;
@@ -29,28 +22,19 @@ public class ShortestReachInGraph {
 
             if (queries == 0) {
                 queries = Integer.parseInt(inputLine);
-            } else if (nodesAndEdges && inputLine.length() > 1) {
-                String[] arr = inputLine.split(" ");
+            } else if (nodesAndEdges && inputLine.contains(SEPARATOR)) {
+                String[] arr = inputLine.split(SEPARATOR);
                 nodes = Integer.parseInt(arr[0]);
                 nodesAndEdges = false;
-            } else if (!nodesAndEdges && inputLine.length() > 1) {
-                String[] temp = inputLine.split(" ");
-                int key = Integer.parseInt(temp[0]);
-                int value = Integer.parseInt(temp[1]);
-                Set<Integer> values;
+            } else if (!nodesAndEdges && inputLine.contains(SEPARATOR)) {
+                String[] temp = inputLine.split(SEPARATOR);
+                int left = Integer.parseInt(temp[0]);
+                int right = Integer.parseInt(temp[1]);
 
-                if (Objects.isNull(pathToNextNodes.get(key))) {
-                    values = new HashSet<>();
-                    values.add(value);
-                    pathToNextNodes.put(key, values);
-                } else {
-                    values = pathToNextNodes.get(key);
-                    values.add(value);
-                    pathToNextNodes.put(key, values);
-                }
-                values.add(value);
-                pathToNextNodes.put(key, values);
-            } else if (inputLine.length() == 1) {
+                pathBetweenNodes(pathToNextNodes, left, right);
+                pathBetweenNodes(pathToNextNodes, right, left);
+
+            } else if (!inputLine.contains(SEPARATOR)) {
                 int startNodeValue = Integer.parseInt(inputLine);
                 int currentNode;
 
@@ -90,7 +74,7 @@ public class ShortestReachInGraph {
                         if (i == nodeList.size() - 1) {
                             stringBuilder.append(count);
                         } else {
-                            stringBuilder.append(count).append(" ");
+                            stringBuilder.append(count).append(SEPARATOR);
                         }
                     }
                 }
@@ -101,5 +85,16 @@ public class ShortestReachInGraph {
             }
         }
         scanner.close();
+    }
+    private static void pathBetweenNodes(Map<Integer, Set<Integer>> pathToNextNodes, int key, int value){
+        Set<Integer> values;
+
+        if (Objects.isNull(pathToNextNodes.get(key))) {
+            values = new HashSet<>();
+        } else {
+            values = pathToNextNodes.get(key);
+        }
+        values.add(value);
+        pathToNextNodes.put(key, values);
     }
 }
