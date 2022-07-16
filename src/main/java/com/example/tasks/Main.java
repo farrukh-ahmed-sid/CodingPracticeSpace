e8dd773563cf57c82e664701e6a97dd6930bd759
farrukh-ahmed-sid <mr.farrukh.ahmed@gmail.com>
2022-07-16 23:11:38 +0500

    update existing code

--- src/main/java/com/example/tasks/Main.java
+++ src/main/java/com/example/tasks/Main.java
@@ -1,23 +1,15 @@
 package com.example.tasks;
 
 
-import com.example.tasks.solutions.list.DoublyLinkedList;
-import com.example.tasks.solutions.list.Node;
+import com.example.tasks.solutions.codility.zalando.MaxEmployAttendSession;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        DoublyLinkedList list = new DoublyLinkedList();
-        Node node = null;
-        node = list.insertNodeInAscending(node, 5);
-        node = list.insertNodeInAscending(node, 4);
-        node = list.insertNodeInAscending(node, 3);
-        node = list.insertNodeInAscending(node, 1);
-        node = list.insertNodeInAscending(node, 2);
-
-        list.printLinkedList(node);
-
+        MaxEmployAttendSession obj = new MaxEmployAttendSession();
+        String[] strings = {"5421", "245", "1452", "0345", "53", "345"};
+        System.out.println(obj.maxAttendees(strings));
     }
 
 }
--- src/main/java/com/example/tasks/solutions/codility/zalando/MaxEmployAttendSession.java
+++ src/main/java/com/example/tasks/solutions/codility/zalando/MaxEmployAttendSession.java
@@ -14,37 +14,35 @@ public class MaxEmployAttendSession {
     will return the maximum number of employees that can attend during at least one of the two scheduled days.
 
     Given E = ["039", "4", "14", "32", "", "34", "7"], the answer is 5.
-    It can be achieved for example by running training on days 3 and 4. This way employees number 0, 1, 2, 3 and 5 will attend the training.
+    It can be achieved for example by running training on days 3 and 4.
+    This way employees number 0, 1, 2, 3 and 5 will attend the training.
 
     Given E = ["801234567", "180234567", "0", "189234567", "891234567", "98", "9"], the answer is 7.
-    It can be achieved for example by running training on days 0 and 9. This way employees all will attend the training.
+    It can be achieved for example by running training on days 0 and 9. This way all employees will attend the training.
 
     Given E = ["5421", "245", "1452", "0345", "53", "345"], the answer is 6.
-    It can be achieved for example by running training once on day 5. This way employees all will attend the training.
+    It can be achieved for example by running training once on day 5. This way all employees will attend the training.
 
     https://stackoverflow.com/questions/72046602/find-max-students-who-can-attend-the-session
     */
 
     public int maxAttendees(String[] E) {
-        int size = E.length;
         int max = 0;
 
-        for (int i = 0; i < size; i++)
-            for (int j = i + 1; j < size; j++) {
-
+        for (int i = 0; i < 10; i++)
+            for (int j = i + 1; j < 10; j++) {
+                //create all pairs of days one by one like 01, 02, 03, 04, 05..... 89
                 String subStr = i + "" + j;
                 int count = 0;
 
-                for (int k = 0; k < size; k++) {
-                    String val = E[k];
+                for (int emp = 0; emp < E.length; emp++) {
+                    String val = E[emp];
 
-                    if (val.matches(".*[" + subStr + "].*")) {
+                    if (val.matches(".*[" + subStr + "].*"))
                         count++;
-                    }
                 }
-                if (count > max) {
+                if (count > max)
                     max = count;
-                }
             }
         return max;
     }
