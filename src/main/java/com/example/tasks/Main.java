54a54937dadca1339cf8b5950cf05a9acbd70bba
farrukh-ahmed-sid <mr.farrukh.ahmed@gmail.com>
2022-08-17 04:22:36 +0500

    shortest path dijkstra algo applied

--- src/main/java/com/example/tasks/Main.java
+++ src/main/java/com/example/tasks/Main.java
@@ -1,17 +1,14 @@
 package com.example.tasks;
 
 
-import com.example.tasks.solutions.graphs.GraphBase;
-
-import java.util.Map;
+import com.example.tasks.solutions.graphs.WeightedGraph;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        GraphBase graphBase = new GraphBase();
-        Map<String, String> map = graphBase.getAdjacencyListFromUndirectedGraph(graphBase.initialize());
-        System.out.println(graphBase.hasPathUndirectedGraph(map, "b", "d"));
+        WeightedGraph weightedGraph = new WeightedGraph();
+        System.out.println(weightedGraph.shortestPath(weightedGraph.initializeDirectionalGraph(), "a", "d"));
 
     }
 
